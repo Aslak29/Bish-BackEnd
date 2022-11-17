cc3a103d11b6fa9d4b966b3567cf2ccf0fa73381
FlorentN <floneuville@gmail.com>
2022-11-17 10:05:27 +0100

    corrections productController

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,18 +3,14 @@
 namespace App\Controller;
 
 use App\Repository\ProduitRepository;
-use Doctrine\DBAL\Types\BooleanType;
-use Doctrine\DBAL\Types\StringType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use OpenApi\Annotations as OA;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\Produit;
 // exporter vers AdminProductView ? - Flo
 #[Route('api/produit')]
 class ProductController extends AbstractController
@@ -48,31 +44,30 @@ class ProductController extends AbstractController
         return new JsonResponse($produitArray);
     }
 
-    #[Route('/', name: 'app_add_product', methods: "POST")]
-    public function addProduit(FormBuilderInterface $addForm){
-        $addForm
-            ->add('nom', StringType::class)
-            ->add('description', TextareaType::class)
-            ->add('pathImage', StringType::class)
-            ->add('price', NumberType::class)
-            ->add('is_trend', BooleanType::class)
-            ->add('is_available', BooleanType::class)
-            ->add('save', SubmitType::class)
-            ;
-    }
-    public function configureOptions(OptionsResolver $resolver){
-        $resolver->setDefaults([
-            'data_class'=>Produit::class,
-        ]);
-    }
-    public function index() : Response
-    {
-        $produit = new Produit();
-        $form = $this->createForm(Produit::class, $produit);
+    /**
+     * @param ProduitRepository $produitRepository
+     * @param Request $request
+     * @return JsonResponse
+     * @OA\Tag (name="Produit")
+     * @OA\Response(
+     *     response="200",
+     *     description = "OK"
+     * )
+     */
 
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'ProductController',
-            'form' => $form->createView()
-        ]);
+    #[Route('/add/{name}/{description}/{pathImage}/{price}/{is_trend}/{is_available}', name: 'app_add_product', methods: "POST")]
+    public function addProduit(ProduitRepository $produitRepository, Request $request){
+        $produit = new Produit();
+        $produit->setName($request->attributes->get('name'));
+        $produit->setDescription($request->attributes->get('description'));
+        $produit->setPathImage($request->attributes->get('pathImage'));
+        $produit->setPrice(floatval( $request->attributes->get('price'))); 
+        $produit->setIsTrend($request->attributes->get('is_trend'));
+        $produit->setIsAvailable($request->attributes->get('is_available'));
+
+        $produitRepository->save($produit,true);
+
+        return new JsonResponse(null,200);
+
     }
 }

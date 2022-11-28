72a28e244437535aead61090df6acbb7b225d119
Aslak29 <alexandre.cholewa@outlook.fr>
2022-11-28 14:56:41 +0100

    searchfilter 1st

--- src/Repository/ProduitRepository.php
+++ src/Repository/ProduitRepository.php
@@ -39,20 +39,28 @@ class ProduitRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Produit[] Returns an array of Produit objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+   /**
+    * @return Produit[] Returns an array of Produit objects
+    */
+   public function findByFilter($orderby,$moyenne,$minprice,$maxprice): array
+   {
+    $entityManager = $this->getEntityManager();
+
+    $query = $entityManager->createQuery(
+        'SELECT p
+        FROM App\Entity\Produit p
+        WHERE p.price BETWEEN :minprice AND :maxprice
+        ORDER BY p.price :orderby
+        '
+    )->setParameters([
+        'minprice'=>$minprice,
+        'maxprice'=>$maxprice,
+        'orderby'=>$orderby
+    ]);
+
+    // returns an array of Product objects
+    return $query->getResult();
+   }
 
 //    public function findOneBySomeField($value): ?Produit
 //    {

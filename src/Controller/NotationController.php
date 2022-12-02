e1427bac187482e472c39723bc3ae6542774f1c3
FlorentN <floneuville@gmail.com>
2022-12-02 09:34:07 +0100

    création Notation Controller

---
+++ src/Controller/NotationController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+use App\Repository\NoteRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use OpenApi\Annotations as OA;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Produit;
+
+        /**
+     * @param NoteRepository $produitRepository
+     * @return JsonResponse
+     * @OA\Tag (name="Produit")
+     * @OA\Response(
+     *     response="200",
+     *     description = "OK"
+     * )
+     */
+    #[Route('/note', name: 'note_produit', methods:"GET")]
+    public function productNotation(NoteRepository $notesRepository): JsonResponse
+    {
+        $notes = $notesRepository->findAll();
+        $noteArray = [];
+        foreach($notes as $note){
+            $noteArray[] = [
+                'id' => $note->getId(),
+                'name' => $note->getName(),
+
+            ];
+        }
+        return new JsonResponse($noteArray);
+    }

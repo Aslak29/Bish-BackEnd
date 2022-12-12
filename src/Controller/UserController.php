9c313d98e5fe98e8a3a7995375e9d6d1c271de12
Aslak29 <alexandre.cholewa@outlook.fr>
2022-12-12 15:50:19 +0100

    read and add user admin

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -95,4 +95,97 @@ class UserController extends AbstractController
         ];
         return new JsonResponse($userArray);
     }
+   /**
+     * @param UserRepository $userRepository
+     * @return JsonResponse
+     * @OA\Tag (name="User")
+     * @OA\Response(
+     *     response="200",
+     *     description = "OK"
+     * )
+     */
+
+     #[Route('/', name: 'user_all', methods:"GET")]
+     public function findProduct(UserRepository $userRepository): JsonResponse
+     {
+         $users = $userRepository->findAll();
+         $userArray = [];
+         foreach($users as $user){
+             $jsonProduct = [
+                 'id' => $user->getId(),
+                 'name' => $user->getName(),
+                 'surname' => $user->getSurname(),
+                 'email' => $user->getEmail(),
+                 'roles' => $user->getRoles(),
+                 'phone' => $user->getPhone(),
+                 'created_at' => $user->getCreatedAt()->format("d-m-Y"),
+             ];
+             $userArray[] = $jsonProduct;
+     }
+     return new JsonResponse($userArray);
+    }
+
+        /**
+     * @param UserRepository $userRepository
+     * @param Request $request
+     * @return JsonResponse
+     * @OA\Tag (name="User")
+     * @OA\Response(
+     *     response="200",
+     *     description = "OK"
+     * )
+     */
+    #[Route('/create/{name}/{surname}/{email}/{password}/{passwordConfirm}/{roles}/{phone}', name: 'user_create', methods: ["POST"])]
+    public function create(UserRepository $userRepository, Request $request, ValidatorInterface $validator): JsonResponse{
+
+        $role = $request->attributes->get('roles');
+        /* Récupération des attributs dans la requètes POST en les settant à la nouvelle entitée User*/
+        $user = new User();
+        $user->setName($request->attributes->get('name'));
+        $user->setSurname($request->attributes->get('surname'));
+        $user->setEmail($request->attributes->get('email'));
+        $user->setPhone($request->attributes->get('phone'));
+
+        $password = $request->attributes->get('password');
+        $passwordConfirm = $request->attributes->get('passwordConfirm');
+        $user->setPassword($password);
+
+        if($role !== 'ROLE_USER' && $role !== 'ROLE_ADMIN'){
+            return new JsonResponse([
+                "errorCode" => "008",
+                "errorMessage" => "Le Role n'existe pas"
+                ],409);
+        }else{
+            $user->setRoles(array($role));
+        }
+        
+
+
+        
+        /* Gestion des erreurs avec ValidatorInterface qui utilise les annotations Assets exemple #[Assert\Email(message: "L'email n'est pas valide.")]*/
+        $errors = $validator->validate($user);
+        if (count($errors) > 0) {
+            $errorsString = (string) $errors;
+            return new JsonResponse($errorsString,400);
+        }
+
+        if ($password === $passwordConfirm) {
+            $user->setPassword($this->encoder->hashPassword($user, $user->getPassword()));
+        }else {
+            return new JsonResponse(["error" => "les mots de passe ne sont pas idendiques"],400);
+        }
+
+        if ($userRepository->findUserByMail($user->getEmail()) != null){
+            return new JsonResponse([
+                "errorCode" => "001",
+                "errorMessage" => "L'adresse email est déjà inscrite dans la base de données"
+                ],409);
+        } ;
+
+        $userRepository->save($user,true);
+
+
+        return new JsonResponse(null,200);
+    }
+
 }

1dd8fd64a7f16c4f0d388bad1bbb1a2372660e1f
Caudron <antoinecaudron8@gmail.com>
2022-12-14 15:19:12 +0100

    Function addPromotion in PromotionController.php && Assert in entity Promotions.php

--- src/Controller/PromotionController.php
+++ src/Controller/PromotionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Promotions;
 use App\Repository\ProduitRepository;
 use App\Repository\PromotionsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use OpenApi\Annotations as OA;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 
 #[Route('/api/promotion')]
@@ -26,7 +28,7 @@ class PromotionController extends AbstractController
     #[Route('/', name: 'app_promotion', methods: ['GET'])]
     public function index(PromotionsRepository $promotionRepository): JsonResponse
     {
-        $promotions =  $promotionRepository->findAll();
+        $promotions = $promotionRepository->findAll();
         $promoArray = [];
 
         foreach ($promotions as $promotion) {
@@ -39,7 +41,52 @@ class PromotionController extends AbstractController
     }
 
     /**
-     * @param PromotionsRepository $promotionRepository
+     * @param PromotionsRepository $promotionsRepository
+     * @param Request $request
+     * @return JsonResponse
+     * @throws \Exception
+     * @OA\Tag (name="Promotion")
+     * @OA\Response(
+     *     response="200",
+     *     description = "OK"
+     * )
+     */
+    #[Route('/add/{remise}/{startdate}/{enddate}', name: 'app_promotion', methods: ['POST'])]
+    public function addPromotion(PromotionsRepository $promotionsRepository, Request $request, ValidatorInterface $validator): JsonResponse
+    {
+
+        $startDate = new \DateTime($request->attributes->get('startdate'));
+        $endDate = new \DateTime($request->attributes->get('enddate'));
+
+        if ($endDate < $startDate) {
+            return new JsonResponse([
+                "errorCode" => "A définir",
+                "errorMessage" => "End date cannot be inferior at start date"
+            ]);
+        } else {
+            $newPromotion = new Promotions();
+            $newPromotion->setRemise($request->attributes->get('remise'));
+            $newPromotion->setDateStart($startDate);
+            $newPromotion->setDateEnd($endDate);
+            $errors = $validator->validate($newPromotion);
+
+            if (count($errors) > 0) {
+                $errorsString = (string)$errors;
+                return new JsonResponse($errorsString);
+            }
+        }
+        $promotionsRepository->save($newPromotion, true);
+
+        return new JsonResponse([
+            "successCode" => "A définir",
+            "successMessage" => "This promotion has been add"
+        ]);
+    }
+
+    /**
+     * @param PromotionsRepository $promotionsRepository
+     * @param ProduitRepository $produitRepository
+     * @param Request $request
      * @return JsonResponse
      * @OA\Tag (name="Promotion")
      * @OA\Response(
@@ -48,9 +95,7 @@ class PromotionController extends AbstractController
      * )
      */
     #[Route('/remove/{id}', name: 'app_promotion_remove', methods: ['DELETE'])]
-    public function
-    removePromotion(PromotionsRepository $promotionsRepository, ProduitRepository $produitRepository, Request $request)
-    :JsonResponse
+    public function removePromotion(PromotionsRepository $promotionsRepository, ProduitRepository $produitRepository, Request $request): JsonResponse
     {
 
         $promotion = $promotionsRepository->find($request->attributes->get('id'));
@@ -61,7 +106,7 @@ class PromotionController extends AbstractController
                 "errorCode" => "A définir",
                 "errorMessage" => "This promotions dont exist"
             ]);
-        }else {
+        } else {
             foreach ($produitsByPromo as $produit) {
                 $produit->setPromotions(null);
             }

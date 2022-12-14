e192e707e63129f6eb7ec2617c45e9a55d70e645
Caudron <antoinecaudron8@gmail.com>
2022-12-14 14:36:23 +0100

    Function findProductByIdPromo in ProduitRepository.php && removePromotion inc Controller

--- src/Controller/PromotionController.php
+++ src/Controller/PromotionController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
+use App\Repository\ProduitRepository;
 use App\Repository\PromotionsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use OpenApi\Annotations as OA;
@@ -13,7 +14,6 @@ use OpenApi\Annotations as OA;
 #[Route('/api/promotion')]
 class PromotionController extends AbstractController
 {
-
     /**
      * @param PromotionsRepository $promotionRepository
      * @return JsonResponse
@@ -29,12 +29,46 @@ class PromotionController extends AbstractController
         $promotions =  $promotionRepository->findAll();
         $promoArray = [];
 
-        foreach ($promotions as $promotion){
+        foreach ($promotions as $promotion) {
             $promoArray[] = [
                 'id' => $promotion->getId(),
                 'remise' => $promotion->getRemise()
             ];
         }
-        return new JsonResponse($promoArray,200);
+        return new JsonResponse($promoArray, 200);
+    }
+
+    /**
+     * @param PromotionsRepository $promotionRepository
+     * @return JsonResponse
+     * @OA\Tag (name="Promotion")
+     * @OA\Response(
+     *     response="200",
+     *     description = "OK"
+     * )
+     */
+    #[Route('/remove/{id}', name: 'app_promotion_remove', methods: ['DELETE'])]
+    public function
+    removePromotion(PromotionsRepository $promotionsRepository, ProduitRepository $produitRepository, Request $request)
+    :JsonResponse
+    {
+
+        $promotion = $promotionsRepository->find($request->attributes->get('id'));
+        $produitsByPromo = $produitRepository->findProductByIdPromo($request->attributes->get('id'));
+
+        if ($promotion === null) {
+            return new JsonResponse([
+                "errorCode" => "A définir",
+                "errorMessage" => "This promotions dont exist"
+            ]);
+        }else {
+            foreach ($produitsByPromo as $produit) {
+                $produit->setPromotions(null);
+            }
+            $promotionsRepository->remove($promotion, true);
+            return new JsonResponse([
+                "This promotion has been remove"
+            ]);
+        }
     }
 }

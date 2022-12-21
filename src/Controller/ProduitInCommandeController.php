f2f315faaec1276d54041e3f331ec8f80be9f7b9
Angélique <88330034+angelwynn1511@users.noreply.github.com>
2022-12-21 10:01:52 +0100

    ProduitInCommandeDelete

--- src/Controller/ProduitInCommandeController.php
+++ src/Controller/ProduitInCommandeController.php
@@ -91,4 +91,38 @@ class ProduitInCommandeController extends AbstractController {
     
     return new JsonResponse($produitArray);
     }
+
+    /**
+     * @param ProduitInCommandeRepository $produitInCommandeRepository
+     * @param Request $request
+     * @return JsonResponse
+     * @OA\Tag (name="ProduitInCommande")
+     * @OA\Response(
+     *     response="200",
+     *     description = "OK"
+     * )
+     */
+    #[Route('/single_order/delete/{id}', name: 'delete_produit_in_commande', methods:"POST")]
+    public function deleteOneProduct(
+        ProduitInCommandeRepository $produitInCommandeRepository,
+        Request $request
+    ): JsonResponse
+    {
+        $deleteProduitInCommande = $produitInCommandeRepository->find($request->attributes->get('id'));
+        if($deleteProduitInCommande != null){
+            $produitInCommandeRepository->remove($deleteProduitInCommande,true);
+        }else{
+            return new JsonResponse([
+                'errorCode' => "013",
+                'errorMessage' => "Ce produit n'existe pas"
+            ],409);
+        }
+        return new JsonResponse([
+            'successCode' => "004",
+            'successMessage' => "Le produit a bien été supprimé de la commande"
+        ],200);
+
+    
+    return new JsonResponse();
+    }
 }

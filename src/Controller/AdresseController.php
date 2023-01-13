39c45ab61d1d60bf982a9f2fb3d8ac5d10ef57a2
Caudron <antoinecaudron8@gmail.com>
2023-01-13 16:27:58 +0100

    create adresse in adresseController.php

--- src/Controller/AdresseController.php
+++ src/Controller/AdresseController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Adresse;
 use App\Repository\AdresseRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -66,6 +68,78 @@ class AdresseController extends AbstractController
                 "successMessage" => "adress has been remove"
             ],200);
         }
+    }
+
+    /**
+     * @param AdresseRepository $adresseRepository
+     * @param Request $request
+     * @return JsonResponse
+     * * @OA\Tag (name="Adresse")
+     *  @OA\Parameter(
+     *     name="userId",
+     *     in="query",
+     *     description="Utilisateur liée à l'adresse",
+     *     @OA\Schema(type="int")
+     * )
+     * @OA\Tag (name="Adresse")
+     * * @OA\Parameter(
+     *     name="city",
+     *     in="query",
+     *     description="Ville de l'adresse",
+     *     @OA\Schema(type="string")
+     * )
+     * @OA\Parameter(
+     *     name="rue",
+     *     in="query",
+     *     description="Rue de l'adresse",
+     *     @OA\Schema(type="string")
+     * )
+     *  @OA\Parameter(
+     *     name="postalCode",
+     *     in="query",
+     *     description="Code postal de l'adresse",
+     *     @OA\Schema(type="string")
+     * )
+     *  @OA\Parameter(
+     *     name="num_rue",
+     *     in="query",
+     *     description="Numéro de rue de l'adresse",
+     *     @OA\Schema(type="string")
+     * )
+     *   @OA\Parameter(
+     *     name="cpm_adresse",
+     *     in="query",
+     *     description="Complément d'adresse de l'adresse",
+     *     @OA\Schema(type="string")
+     * )
+     *  @OA\Parameter(
+     *     name="name",
+     *     in="query",
+     *     description="Nom de l'adresse",
+     *     @OA\Schema(type="string")
+     * )
+     * @OA\Response(
+     *     response="200",
+     *     description = "OK"
+     * )
+     */
+    #[Route('/create', name: 'app_route_adresse', methods: "POST")]
+    public function create(AdresseRepository $adresseRepository, UserRepository $userRepository, Request $request):JsonResponse{
+
+        $adresse = new Adresse();
+        $adresse->setCity($request->query->get("city"));
+        $adresse->setRue($request->query->get("rue"));
+        $adresse->setNumRue($request->query->get("num_rue"));
+        $adresse->setComplementAdresse($request->query->get("cpm_adresse"));
+        $adresse->setName($request->query->get("name"));
+        $adresse->setCity($request->query->get("city"));
+        $adresse->setPostalCode(($request->query->get("postalCode")));
+        $adresse->setUser($userRepository->find($request->query->get("userId")));
+
+        $adresseRepository->save($adresse,true);
+
+        return new JsonResponse();
+
     }
 
 }

fb7f8ea9ca70816c267e29096890004c7df1525f
corentin27 <tailleur.corentin27@gmail.com>
2023-01-17 10:03:33 +0100

    Mise en place du Service

--- src/Controller/NotationController.php
+++ src/Controller/NotationController.php
@@ -2,19 +2,34 @@
 
 namespace App\Controller;
 use App\Repository\NoteRepository;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use App\Services\NoteService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use OpenApi\Annotations as OA;
-use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Produit;
 
 #[Route('api/notation')]
-class NotationController extends AbstractController{
-        /**
-     * @param NoteRepository $produitRepository
+class NotationController extends AbstractController
+{
+    private NoteService $noteService;
+
+
+    /**
+     * @param NoteService $noteService
+     */
+    public function __construct(NoteService $noteService)
+    {
+        $this->noteService = $noteService;
+    }
+
+
+    /**
+     * @param NoteRepository $notesRepository
      * @return JsonResponse
-     * @OA\Tag (name="Produit")
+     * @OA\Tag (name="Note")
      * @OA\Response(
      *     response="200",
      *     description = "OK"
@@ -25,13 +40,35 @@ class NotationController extends AbstractController{
     {
         $notes = $notesRepository->findAll();
         $noteArray = [];
-        foreach($notes as $note){
+        foreach ($notes as $note) {
             $noteArray[] = [
                 'id' => $note->getId(),
-                'name' => $note->getName(),
-
+                'note' => $note->getNote(),
+                'nameProduct' => $note->getProduit()->getName(),
             ];
         }
         return new JsonResponse($noteArray);
     }
+
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @OA\Tag (name="Note")
+     * @OA\Response(
+     *     response="200",
+     *     description = "OK"
+     * )
+     */
+    #[Route('/noteUser/{id}/{idProduct}/{value}', name: 'note_update_produit', methods:"POST")]
+    public function noteByUser(Request $request): JsonResponse
+    {
+        $userId = $request->attributes->get('id');
+        $productId = $request->attributes->get('idProduct');
+        $noteValue = $request->attributes->get('value');
+        $note = $this->noteService->updateNoteByUser($userId, $productId, $noteValue);
+
+        return new JsonResponse($note);
+    }
+
 }

4b8089b532b14a0e08ab0c5c6131eb76d40a142e
Caudron <antoinecaudron8@gmail.com>
2023-01-19 15:53:47 +0100

    method to update paymentMethod in PaymentIntent && method to get paymentMethod

--- src/Controller/StripeController.php
+++ src/Controller/StripeController.php
@@ -28,12 +28,12 @@ class StripeController extends AbstractController
         $amount = $request->attributes->get("amount");
 
         $stripe = new StripeClient('sk_test_51LwmsKBjYw0WvT4HhDKEhAOoXseKSd0B2JhUhd4eoF2NyrGyA79rOc7VfK4KvSRJLlpweRv7HHQVOsgHrP9VPRAo008FNyvIbg'); //TODO Put the secret key in .env
-        $stripe->paymentIntents->create(
+        $paymentIntent = $stripe->paymentIntents->create(
             ['amount' => $amount * 100, 'currency' => 'usd', 'payment_method_types' => ['card']]
         );
 
         return new JsonResponse([
-            "successMessage" => "The payment intent is a success. The secret client save in server."
+            "id" => $paymentIntent->id
         ]);
     }
 
@@ -45,7 +45,7 @@ class StripeController extends AbstractController
      * )
      * @throws ApiErrorException
      */
-    #[Route('/clientSecret/{idPaymentIntent}',name: 'app_stripe_clientSecret',methods: ['GET'])]
+    #[Route('/clientSecret/{idPaymentIntent}',name: 'app_stripe_clientSecret',methods: ['POST'])]
     public function getClientSecret(Request $request): JsonResponse{
 
         $idPaymentIntent = $request->attributes->get('idPaymentIntent');
@@ -57,4 +57,44 @@ class StripeController extends AbstractController
             "clientSecret" => $paymentIntent->client_secret
         ]);
     }
+
+
+    /**
+     * @OA\Tag (name="Stripe")
+     * @OA\Response(
+     *     response="200",
+     *     description = "OK"
+     * )
+     * @throws ApiErrorException
+     */
+    #[Route('/PaymentMethod/{idPaymentMethod}', name:'app_stripe_paymentMethod', methods:['POST'])]
+    public function paymentMethod(Request $request): JsonResponse{
+
+        $stripe = new StripeClient('sk_test_51LwmsKBjYw0WvT4HhDKEhAOoXseKSd0B2JhUhd4eoF2NyrGyA79rOc7VfK4KvSRJLlpweRv7HHQVOsgHrP9VPRAo008FNyvIbg'); //TODO Put the secret key in .env
+        $paymentMethods = $stripe->paymentMethods->retrieve($request->attributes->get('idPaymentMethod'));
+        return new JsonResponse($paymentMethods,200);
+    }
+
+    /**
+     * @OA\Tag (name="Stripe")
+     * @OA\Response(
+     *     response="200",
+     *     description = "OK"
+     * )
+     * @throws ApiErrorException
+     */
+    #[Route('/setPaymentIntent/PaymentMethod/{idPaymentIntent}/{idPaymentMethod}', name:'app_stripe_update_PaymentIntent', methods:['POST'])]
+    public function updatePaymentMethodInPaymentIntent(Request $request): JsonResponse{
+
+        $stripe = new StripeClient('sk_test_51LwmsKBjYw0WvT4HhDKEhAOoXseKSd0B2JhUhd4eoF2NyrGyA79rOc7VfK4KvSRJLlpweRv7HHQVOsgHrP9VPRAo008FNyvIbg'); //TODO Put the secret key in .env
+
+        $paymentIntent = $stripe->paymentIntents->update($request->attributes->get('idPaymentIntent'),[
+            'payment_method' => $request->attributes->get('idPaymentMethod')
+        ]);
+
+        return new JsonResponse($paymentIntent,200);
+    }
+
+
+
 }

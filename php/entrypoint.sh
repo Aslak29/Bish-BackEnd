ab66f51c5c48c53699d88fa6b700e70994ba26a9
Romain H <romain.hermant62@gmail.com>
2023-02-13 09:14:03 +0100

    fix

--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -3,6 +3,4 @@ php bin/console doctrine:database:create
 php bin/console make:migration
 php bin/console doctrine:migration:migrate --no-interaction
 php bin/console doctrine:fixture:load --no-interaction
-
-
 exec "$@"
